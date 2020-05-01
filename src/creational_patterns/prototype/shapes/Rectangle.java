9e301eeb4c5da6418f776d12988c502221b69079
jaysonzanarias <jayson.zanarias@gmail.com>
2020-05-01 22:12:38 +0800

    Creational Patterns: Prototype

---
+++ src/creational_patterns/prototype/shapes/Rectangle.java
@@ -0,0 +1,33 @@
+package creational_patterns.prototype.shapes;
+
+public class Rectangle extends Shape {
+    public int width;
+    public int height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(Rectangle target) {
+        super(target);
+        if (target != null) {
+            this.width = target.width;
+            this.height = target.height;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Rectangle) || !super.equals(object2)) {
+            return false;
+        }
+
+        Rectangle shape2 = (Rectangle) object2;
+        System.out.println("RECTANGLE");
+        return shape2.width == width && shape2.height == height;
+    }
+}
